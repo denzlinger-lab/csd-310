35de1af1232c08dc174dc02d0ccd325bb388380e
denzlinger-lab <adenzlinger1025@gmail.com>
2025-11-22 19:55:04 -0600

    11/22/2025 push

---
+++ module-6/mysql_test.py
@@ -0,0 +1,43 @@
+"""import statements"""
+import mysql.connector  # to connect
+from dotenv import dotenv_values
+from mysql.connector import errorcode
+
+#using our .env files
+secrets = dotenv_values(".env")
+
+"""database config object"""
+config = {
+    "user": secrets["USER"],
+    "password": secrets["PASSWORD"],
+    "host": secrets["HOST"],
+    "database": secrets["DATABASE"],
+    "raise_on_warnings": True #not in .env file
+}
+
+try:
+    """try/catch block for handling potential mysql database errors"""
+
+    db = mysql.connector.connect(**config) # connect to movies database
+
+    # output the connection status
+    print("\n Database user {} connected to MySql on host with database {}".format(config["user"], config["host"], config["database"]))
+
+    input("\n\n Press any key to continue...")
+
+except mysql.connector.Error as err:
+    """on error code"""
+
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print(" The supplied username and password are invalid")
+
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print(" The specified database does not exist")
+
+    else:
+        print(err)
+
+finally:
+    """close the connection to MySql"""
+
+    db.close()
